53eacba54539d1fe77380d54c5e6d364f3b562ef
alanbanks229 <alanbanks229@gmail.com>
2020-04-09 14:31:49 -0400

    updated backend

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,14 @@
 class RoomsController < ApplicationController
+
+
+    def create 
+        user = User.find_or_create_by(firebase_id: params[:firebase_id])
+        room = Room.create(max_number: params[:maxPeople], status: "open", room_number: params[:randomRoomNumber])
+        user_room = UserRoom.create(user: user, room: room, host: true)
+        render json: ({user: user, room: room, user_room: user_room}).to_json()
+        # render json: user.to_json()
+    end
+
+
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,25 @@ class UsersController < ApplicationController
     def index
         users = User.all
         render json: users.to_json()
+
     end
 
     def create
-        debugger
+        
+    end
+
+    def show
+
+        user = User.find(params[:id])
+        render json: user.to_json()
+
     end
 
     
+    private
+
+    def user_params
+        params.require(:user).permit(:firebase_id, :usernam)
+    end
 
 end
