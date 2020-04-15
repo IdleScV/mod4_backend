199000f95269ce0468bfa24b6511b1be25b4bf87
alanbanks229 <alanbanks229@gmail.com>
2020-04-15 09:50:25 -0400

    updated user showpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,12 @@ class UsersController < ApplicationController
     def show
 
         user = User.find_by(params[:id])
-        user.drawings
+        drawings = users.drawings
+        reviews = drawings.map{|d| d.reviews}
+        prompt = drawings.map{|d| d.prompt_datum}
+
         if user  
-            render json: {drawings: user.drawings, username: user.username}
+            render json: {username: user.username, drawings: drawingss,  reviews: reviews, prompt: prompt}
         else 
             render json: {message: "user has not played any games"}
         end
